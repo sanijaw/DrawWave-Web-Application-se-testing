4a6045f8365a6ee050db9d7fbc25b6503d1f47df
Yevin Mawathage <mynsmedonza@students.nsbm.ac.lk>
2025-05-21 04:38:19 +0530

    back to htttp

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:["https://app.drawwave.space", "http://localhost:5173"] ,
+  origin:["http://app.drawwave.space", "http://localhost:5173"] ,
   credentials: true, // Important for authentication
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
